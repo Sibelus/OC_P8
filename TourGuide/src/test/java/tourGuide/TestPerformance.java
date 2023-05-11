e07c2ea5a9b457575a49f9a2328518ccae7d27b9
4H0 <ggaetan@proton.me>
2023-05-11 08:47:29 +0200

    Remove useless annotation & setProximityBuffer(10)

--- TourGuide/src/test/java/tourGuide/TestPerformance.java
+++ TourGuide/src/test/java/tourGuide/TestPerformance.java
@@ -46,8 +46,7 @@ public class TestPerformance {
 	 */
 
 
-	
-	//@Ignore
+
 	@Test
 	public void highVolumeTrackLocation() {
 		//GIVEN
@@ -90,13 +89,14 @@ public class TestPerformance {
 		System.out.println("highVolumeTrackLocation: Time Elapsed: " + TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()) + " seconds."); 
 		assertTrue(TimeUnit.MINUTES.toSeconds(15) >= TimeUnit.MILLISECONDS.toSeconds(stopWatch.getTime()));
 	}
-	
-	//@Ignore
+
+
 	@Test
 	public void highVolumeGetRewards() {
 		//GIVEN
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
+		rewardsService.setProximityBuffer(10);
 
 		// Users should be incremented up to 100,000, and test finishes within 20 minutes
 		InternalTestHelper.setInternalUserNumber(100000);
@@ -108,8 +108,6 @@ public class TestPerformance {
 		List<User> allUsers = new ArrayList<>();
 		allUsers = tourGuideService.getAllUsers();
 		allUsers.forEach(u -> u.addToVisitedLocations(new VisitedLocation(u.getUserId(), attraction, new Date())));
-	     
-	    //allUsers.forEach(u -> rewardsService.calculateRewardsAsync(u));
 
 
 		//WHEN
